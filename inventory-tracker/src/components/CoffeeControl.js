c7899059482bb730d3aa26e18d4f5c467ba0af68
Zachary <ZackCipolletta@gmail.com>
2023-04-21 10:37:22 -0700

    Add basic boilerplate to each file, update App.js to import Header and CoffeeControl. Create initial CoffeeControl class

---
+++ inventory-tracker/src/components/CoffeeControl.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class CoffeeControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      coffeeList: []
+    };
+  }
+
+
+
+
+
+}
+
+export default CoffeeControl;
